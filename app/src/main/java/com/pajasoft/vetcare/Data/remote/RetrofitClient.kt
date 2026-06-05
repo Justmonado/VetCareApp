7669d23288629a07ef667a90d19fb79c5c862790
ajcastuerag-ux <ajcastuerag@gmail.com>
2026-06-04 22:59:29 -0600

    RetrofitClient

---
+++ app/src/main/java/com/pajasoft/vetcare/Data/remote/RetrofitClient.kt
@@ -0,0 +1,32 @@
+package com.pajasoft.vetcare.Data.remote
+
+import com.pajasoft.vetcare.Data.local.TokenManager
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import java.util.concurrent.TimeUnit
+
+object RetrofitClient {
+    const val BASE_URL = "http://172.20.32.128:3000/api/"
+
+    fun create(tokenManager: TokenManager): ApiService {
+        val logging = HttpLoggingInterceptor().apply {
+            level = HttpLoggingInterceptor.Level.BODY
+        }
+
+        val client = OkHttpClient.Builder()
+            .addInterceptor(AuthInterceptor(tokenManager))
+            .addInterceptor(logging)
+            .connectTimeout(20, TimeUnit.SECONDS)
+            .readTimeout(20, TimeUnit.SECONDS)
+            .build()
+
+        return Retrofit.Builder()
+            .baseUrl(BASE_URL)
+            .client(client)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+            .create(ApiService::class.java)
+    }
+}
